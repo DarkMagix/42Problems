3616a9e685df12ba3af6fbe77ccb5c8bd94b06e2
root <root@DESKTOP-AUK10SC.localdomain>
2018-05-15 02:29:58 -0700

    Ft_itoa_base

--- ExamPractice/ft_itoa_base/ft_itoa_base.c
+++ ExamPractice/ft_itoa_base/ft_itoa_base.c
@@ -1,58 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/17 12:43:43 by exam              #+#    #+#             */
-/*   Updated: 2018/04/19 19:31:54 by mweir            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include "stdlib.h"
+
+int numLen(int nbr, int base)
+{
+  int size;
+
+  size = (nbr < 0) ? 1 : 0;
+  while (nbr != 0)
+    {
+      size++;
+      nbr/=base;
+    }
+  return (size-1);
+}
+
+int ft_abs(int nbr)
+{
+  return ((nbr < 0) ? -nbr : nbr);
+}
 
 char *ft_itoa_base(int nbr, int base)
 {
-	char *hex = "0123456789ABCDEF";
-    long sum;
-    int neg;
-    int len;
-    long tempN;
-    char *res;
-	sum = nbr;
-    neg = 0;
-    len = 1;
-	if (base <2)
-		return (NULL);
-    if (sum < 0)
-    {
-        neg = 1;
-        sum *=-1;
+  int len;
+  char *res;
+  int neg;
+  char *hex;
+  long long int value;
+  
+  hex = "0123456789ABCDEF";
+  value = nbr;
+  if (value == 0 || base <2)
+    return ("0");
+  len = numLen(value, base);
+  neg = (value < 0) ? 1 : -1;
+  res = (char*)malloc(sizeof(char) * (len + 1));
+  len--;
+  res[len]= '\0';
+  while (len >= 0)
+    {      
+      res[len] = hex[ft_abs(value % base)];
+      value = ft_abs(value / base);
+      len--;
     }
-	if(base ==10)
-		len++;
-    tempN = sum;
-    while (tempN >= base)
-    {
-        len++;
-        tempN/=base;
-    }
-    res = malloc(sizeof(char) * (len + 1));
-    res[len] = '\0';
-    while (len--)
-    {
-        res[len] = hex[sum % base];
-        sum /= base;
-    }
-    if (neg == 1 && base ==10)
-        res[0] = '-';
-    return (res);
-}
-
-int main(void)
-{
-	printf("%s", ft_itoa_base(0,2));
-	return (0);
+  if (neg == -1 && base ==10)
+    res[0]='-';
+  return (res);
 }

b982657f3214096c021ff5a65008b352bde56a18
DarkMagix <mweir@e1z3r4p20.42.us.org>
2018-07-23 18:44:22 -0700

    Ft Itoa Base

--- ExamPractice/ft_itoa_base/ft_itoa_base.c
+++ ExamPractice/ft_itoa_base/ft_itoa_base.c
@@ -1,72 +1,53 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_itoa_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: mweir <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/17 12:43:43 by exam              #+#    #+#             */
-/*   Updated: 2018/04/22 17:30:08 by mweir            ###   ########.fr       */
+/*   Created: 2018/07/23 18:43:55 by mweir             #+#    #+#             */
+/*   Updated: 2018/07/23 18:44:05 by mweir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-=======
-#include <unistd.h>
->>>>>>> 84d67bf0173085b49fa772c6f3d1582575cebfac
-#include <stdio.h>
-#include "stdlib.h"
-
-int numLen(int nbr, int base)
+int is_empty(char c)
 {
-  int size;
-
-  size = (nbr < 0) ? 1 : 0;
-  while (nbr != 0)
-    {
-      size++;
-      nbr/=base;
-    }
-  return (size-1);
-}
-
-int ft_abs(int nbr)
-{
-  return ((nbr < 0) ? -nbr : nbr);
-}
-
-<<<<<<< HEAD
-int main(int argc, char **argv)
-{
-	printf("%s", ft_itoa_base(atoi(argv[1]), atoi(argv[2])));
+	if (c <= 32)
+		return (1);
 	return (0);
-=======
-char *ft_itoa_base(int nbr, int base)
+}
+
+int is_nbr(char c, int base)
 {
-  int len;
-  char *res;
-  int neg;
-  char *hex;
-  long long int value;
-  
-  hex = "0123456789ABCDEF";
-  value = nbr;
-  if (value == 0 || base <2)
-    return ("0");
-  len = numLen(value, base);
-  neg = (value < 0) ? 1 : -1;
-  res = (char*)malloc(sizeof(char) * (len + 1));
-  len--;
-  res[len]= '\0';
-  while (len >= 0)
-    {      
-      res[len] = hex[ft_abs(value % base)];
-      value = ft_abs(value / base);
-      len--;
-    }
-  if (neg == -1 && base ==10)
-    res[0]='-';
-  return (res);
->>>>>>> 84d67bf0173085b49fa772c6f3d1582575cebfac
+	char l_hex[16] = "0123456789abcdef";
+	char u_hex[16] = "0123456789ABCDEF";
+	while (base--)
+		if (l_hex[base] == c || u_hex[base] == c)
+			return (1);
+	return (0);
+}
+int get_val(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (c - '0');
+	else if (c >= 'a' && c <= 'f')
+		return (c - 'a' + 10);
+	else if (c >= 'A' && c <= 'F')
+		return (c - 'A' + 10);
+	return (0);
+}
+
+int    ft_atoi_base(const char *str, int str_base)
+{
+	int sum;
+	int neg;
+
+	sum = 0;
+	while (is_empty(*str))
+		str++;
+	neg = (*str == '-') ? -1 : 1;
+	(*str == '-' || *str == '+') ? str++ : 0;
+	while (is_nbr(*str, str_base))
+		sum = sum * str_base + get_val(*str++);
+	return (sum * neg);
 }
